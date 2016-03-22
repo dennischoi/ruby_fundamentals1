e320f4846ccf8705dac07e826fa03d48b4c6f800
CJ <dennischoi@Denniss-MacBook.local>
2016-03-22 14:37:37 -0400

    Exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,11 @@ puts "$#{55*0.2}" # 20% tip if your generous...
 puts "Only " + "1" + " Lord of the Ring..."  # Just put the integer in quotes
 puts "#{45628 * 7839}"
 puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+
+# Examples....
+my_variable = 'my_variable is now this string'
+# irb exmaples... of assigning variables
+# irb examples of increments
+# irb examples of variables and inserting default values
+        # variable ||=   assigns a default value of a variable isnt given any values
