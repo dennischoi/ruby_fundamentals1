8799339c5728800df3fafd9c310732625c2e097d
CJ <dennischoi@Denniss-MacBook.local>
2016-03-22 14:56:15 -0400

    Age and year born

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,9 @@ user_input = gets.chomp
 # get.chomp takes out white spaces
 
 puts "Hi #{user_input}!"
+
+puts "How old are you?"
+
+user_age = gets.chomp.to_i
+year = 2015
+puts "Oh you are #{user_age}, born in #{2015 - user_age.to_i}"
