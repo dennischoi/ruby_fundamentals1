7cf5e53cd42896ea02cc4d966c103c0f5b2ebc12
CJ <dennischoi@Denniss-MacBook.local>
2016-03-22 15:26:59 -0400

    Exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,4 +1,12 @@
 nums = (1..100)
 nums.each do |num|
-  puts num
+  if num % 3 == 0 && num % 5 == 0
+    puts "BitMaker"
+  elsif num % 5 == 0
+    puts "Maker"
+  elsif num % 3 == 0
+    puts "Bit"
+  else
+    puts num
+  end
 end
