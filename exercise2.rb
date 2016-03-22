76777dd22941e5b387d9d74dfe7bacaaea058ac0
CJ <dennischoi@Denniss-MacBook.local>
2016-03-22 14:36:44 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,4 @@
+puts "$#{55*0.2}" # 20% tip if your generous...
+puts "Only " + "1" + " Lord of the Ring..."  # Just put the integer in quotes
+puts "#{45628 * 7839}"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
